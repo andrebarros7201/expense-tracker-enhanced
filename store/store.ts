721ebd77d5209357e7cfd859d12054cae1bbb80c
André Barros <andrebarros7201@gmail.com>
2025-02-13 22:27:15 +0000

    add investmentSlice to store

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import budgetSlice from "@/store/budgetSlice";
+import investmentSlice from "@/store/investmentSlice";
 
-const store = configureStore({ reducer: { budget: budgetSlice } });
+const store = configureStore({
+  reducer: { budget: budgetSlice, investment: investmentSlice },
+});
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
