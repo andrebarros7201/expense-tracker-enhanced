083d808f0f0abec92f5f33169a3d31a1cf8ca1b1
André Barros <andrebarros7201@gmail.com>
2025-02-13 15:30:54 +0000

    create investmentSlice tests

---
+++ store/investmentSlice.test.ts
@@ -0,0 +1,36 @@
+import { it, describe, expect } from "vitest";
+import investmentReducer, {
+  calculatePrediction,
+} from "@/store/investmentSlice";
+import InvestmentState from "@/types/investmentState";
+
+describe("Investment Slice", () => {
+  const initialState: InvestmentState = {
+    years: 0,
+    initialAmount: 0,
+    monthlyContribution: 0,
+    yearlyGrowth: 0,
+    prediction: [],
+  };
+
+  it("should return initial state", () => {
+    expect(investmentReducer(undefined, { type: "UNKNOWN_ACTION" })).toEqual(
+      initialState,
+    );
+  });
+
+  it("should change the state", () => {
+    const state = investmentReducer(
+      initialState,
+      calculatePrediction({
+        years: 1,
+        initialAmount: 500,
+        monthlyContribution: 0,
+        yearlyGrowth: 0,
+      }),
+    );
+    expect(state.years).toEqual(1);
+    expect(state.prediction).toHaveLength(1);
+    expect(state.prediction[0].value).toEqual(500);
+  });
+});
