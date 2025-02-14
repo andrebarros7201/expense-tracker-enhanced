536ed86faf2a102d74e434c1607579813bdebc35
André Barros <andrebarros7201@gmail.com>
2025-02-14 14:53:31 +0000

    create home card component

---
+++ components/ui/home-card.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  buttonText: string;
+  buttonLink: string;
+};
+
+export const HomeCard = ({
+  title,
+  description,
+  image,
+  imageAlt,
+  buttonText,
+  buttonLink,
+}: Props) => {
+  return (
+    <div className="bg-gray-100 rounded-xl p-6 shadow-md shadow-gray-300 flex items-center gap-6">
+      <Image
+        src={image}
+        alt={imageAlt}
+        width={128}
+        height={128}
+        className="object-contain rounded-lg"
+      />
+      <div className="flex flex-col gap-2 text-gray-800">
+        <h4 className="text-2xl font-bold">{title}</h4>
+        <p className="text-gray-600">{description}</p>
+        <Link href={buttonLink} className="mt-2">
+          <button className="py-2 px-6 rounded-lg bg-blue-500 text-white font-medium hover:bg-blue-600 hover:-translate-y-1 transition-all duration-300 shadow-md">
+            {buttonText}
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
