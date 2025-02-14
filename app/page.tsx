1ccf0938f8d631dfae09fdf11ea9de2f617bcfed
André Barros <andrebarros7201@gmail.com>
2025-02-14 16:42:34 +0000

    adjust home responsiveness to smaller screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={"flex flex-1 flex-col items-center justify-start"}>
       <header className={" md:mt-12 flex items-center justify-center p-4"}>
-        <div className="w-full max-w-4xl flex flex-col gap-8 items-center justify-center">
+        <div className="w-full flex flex-col gap-8 items-center justify-center">
           <h2 className="text-2xl text-center font-bold italic md:text-3xl lg:text-6xl">
             Expense Tracker Enhanced
           </h2>
@@ -15,7 +15,7 @@ export default function Home() {
           "w-full p-4 flex-1 flex flex-col items-center justify-start xl:h-1/2 pt-4 gap-8"
         }
       >
-        <div className={"flex flex-col md:grid grid-cols-2 gap-8 md:mt-12"}>
+        <div className={"flex flex-col md:grid grid-cols-2 gap-8 md:mt-12 "}>
           <HomeCard
             title={"Create a monthly budget"}
             description={"See where your money goes"}
