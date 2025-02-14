74eb838162ba2b69ca2141d9dc2c3daeea638fa0
André Barros <andrebarros7201@gmail.com>
2025-02-14 15:07:28 +0000

    improve home card to smaller devices

--- components/ui/home-card.tsx
+++ components/ui/home-card.tsx
@@ -19,16 +19,14 @@ export const HomeCard = ({
   buttonLink,
 }: Props) => {
   return (
-    <div className="bg-gray-100 rounded-xl p-6 shadow-md shadow-gray-300 flex items-center gap-6">
-      <Image
+    <div className="bg-gray-100 rounded-xl p-6 shadow-md shadow-gray-300 flex flex-col md:flex-row items-center gap-6">
+      <img
         src={image}
         alt={imageAlt}
-        width={128}
-        height={128}
-        className="object-contain rounded-lg"
+        className="object-contain w-1/4 md:w-1/3"
       />
-      <div className="flex flex-col gap-2 text-gray-800">
-        <h4 className="text-2xl font-bold">{title}</h4>
+      <div className="flex items-center justify-center md:items-start flex-col gap-2 text-gray-800">
+        <h4 className="text-lg lg:text-2xl font-bold">{title}</h4>
         <p className="text-gray-600">{description}</p>
         <Link href={buttonLink} className="mt-2">
           <button className="py-2 px-6 rounded-lg bg-blue-500 text-white font-medium hover:bg-blue-600 hover:-translate-y-1 transition-all duration-300 shadow-md">
