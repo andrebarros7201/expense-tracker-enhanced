f6aef0d4e46b9d1433b45c3ced6a08f58f589303
André Barros <andrebarros7201@gmail.com>
2025-02-14 19:23:55 +0000

    improve Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,15 +1,29 @@
 interface Props {
-  placeholder: string;
+  placeholder?: string;
   type: string;
+  name: string;
+  min?: number;
+  max?: number;
 }
 
-export const Input = ({ placeholder, type }: Props) => {
+export const Input = ({ placeholder, type, name, min, max }: Props) => {
   return (
-    <input
-      type={type}
-      required
-      placeholder={placeholder}
-      className="p-4 bg-white text-black"
-    />
+    <div className={"flex flex-col items-start gap-2 "}>
+      <label htmlFor={name} className={"capitalize"}>
+        {name}
+      </label>
+      <input
+        type={type}
+        required
+        id={name}
+        name={name}
+        min={min}
+        max={max}
+        placeholder={placeholder}
+        className="py-2 px-2 bg-white rounded w-full text-black border-2 box-border border-blue-500 placeholder:italic  appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-appearance:textfield]"
+      />
+    </div>
   );
 };
+
+export default Input;
