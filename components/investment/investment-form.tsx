a13dcea994de60dd2682dd05e043ab2be96da896
André Barros <andrebarros7201@gmail.com>
2025-02-15 22:06:07 +0000

    improve investment form responsiveness

--- components/investment/investment-form.tsx
+++ components/investment/investment-form.tsx
@@ -7,7 +7,9 @@ const InvestmentForm = () => {
         "border-2 border-blue-500 w-full flex flex-col p-4 bg-gray-100 rounded-md shadow-xl"
       }
     >
-      <form className={"gap-4 flex flex-col md:grid grid-cols-5"}>
+      <form
+        className={"gap-4 flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-5"}
+      >
         <Input
           type={"number"}
           name={"years"}
@@ -36,7 +38,7 @@ const InvestmentForm = () => {
         />
         <button
           className={
-            "bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600 transition-all duration-300 shadow-md "
+            "bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600 transition-all duration-300 shadow-md sm:col-span-2 md:col-span-1"
           }
         >
           Calculate
