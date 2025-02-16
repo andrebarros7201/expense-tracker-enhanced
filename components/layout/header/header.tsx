693430c4a1cbd3430026d90ae90609fd957ee495
André Barros <andrebarros7201@gmail.com>
2025-02-16 20:39:56 +0000

    change header item color depending on path

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,19 +1,22 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const path = usePathname();
   return (
     <header className={"w-full flex items-center justify-center p-4 shadow"}>
       <div
         className={"flex items-center justify-center w-full max-w-4xl gap-4"}
       >
-        <h2 className={" text-xl lg:text-2xl italic font-bold"}>
+        <h2 className={"text-xl lg:text-2xl italic font-bold"}>
           <Link href={"/"}>Expense Tracker</Link>
         </h2>
         <ul className={"flex justify-start items-center gap-4"}>
-          <li>
+          <li className={`${path === "/budget" ? "text-blue-500" : ""}`}>
             <Link href={"/budget"}>Monthly Budget</Link>
           </li>
-          <li>
+          <li className={`${path === "/investment" ? "text-blue-500" : ""}`}>
             <Link href={"/investment"}>Investment Calculator</Link>
           </li>
         </ul>
