dd265dab35011253a3b6950d32a2cc48dd03e7eb
André Barros <andrebarros7201@gmail.com>
2025-02-18 14:10:32 +0000

    display chart

--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -1,13 +1,14 @@
 import InvestmentForm from "@/components/layout/investment/investment-form";
 import DisplayYears from "@/components/layout/investment/display-years";
 import Stats from "@/components/layout/investment/stats";
+import Chart from "@/components/layout/investment/chart";
 
 export default function InvestmentPage() {
   return (
     <div className={"w-full flex flex-1 pt-8 flex-col gap-4 "}>
       <InvestmentForm />
       <div className={"flex flex-col sm:grid grid-cols-2 gap-4"}>
-        <Stats />
+        <Chart />
         <Stats />
       </div>
       <DisplayYears />
---
+++ components/layout/investment/chart.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const Chart = () => {
+  const { prediction } = useSelector((state: RootState) => state.investment);
+  const chartData = {
+    labels: prediction.map((item) => item.year),
+    datasets: [
+      {
+        label: "Investment Value",
+        data: prediction.map((item) => item.value),
+        backgroundColor: "rgba(59, 130, 246, 0.5)",
+        borderWidth: 1,
+        borderColor: `rgb(59, 130, 246)`,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { display: true },
+      tooltip: { enabled: true },
+    },
+  };
+  return (
+    <section
+      className={
+        "w-full flex items-center justify-center border-2 border-blue-500 bg-gray-100 rounded-md shadow-xl p-4"
+      }
+    >
+      <Bar data={chartData} options={options} />
+    </section>
+  );
+};
+export default Chart;
