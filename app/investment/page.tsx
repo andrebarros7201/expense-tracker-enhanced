7d262a10db4267de080b8b1692bb09a8ac773130
André Barros <andrebarros7201@gmail.com>
2025-02-18 13:41:49 +0000

    display stats component

--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -1,10 +1,15 @@
 import InvestmentForm from "@/components/layout/investment/investment-form";
 import DisplayYears from "@/components/layout/investment/display-years";
+import Stats from "@/components/layout/investment/stats";
 
 export default function InvestmentPage() {
   return (
     <div className={"w-full flex flex-1 pt-8 flex-col gap-4 "}>
       <InvestmentForm />
+      <div className={"flex flex-col sm:grid grid-cols-2 gap-4"}>
+        <Stats />
+        <Stats />
+      </div>
       <DisplayYears />
     </div>
   );
---
+++ components/layout/investment/stats.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+
+const Stats = () => {
+  const { stats } = useSelector((state: RootState) => state.investment);
+  return (
+    <section
+      className={
+        "w-full border-2 border-blue-500 rounded-md shadow-xl p-4 flex flex-col items-center bg-gray-100 gap-4"
+      }
+    >
+      <p>
+        Total Invested:{" "}
+        {String(stats.totalInvested).replace(/\B(?=(\d{3})+(?!\d))/g, " ")} $
+      </p>
+      <p>
+        Initial Amount:{" "}
+        {String(stats.initialInvestment).replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        $
+      </p>
+      <p>
+        Final Value:{" "}
+        {String(stats.finalValue).replace(/\B(?=(\d{3})+(?!\d))/g, " ")} $
+      </p>
+      <p>
+        Total Growth:{" "}
+        {String(stats.totalGrowth).replace(/\B(?=(\d{3})+(?!\d))/g, " ")} $
+      </p>
+    </section>
+  );
+};
+
+export default Stats;
